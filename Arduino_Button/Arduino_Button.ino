5dc60b719ee381904dcb576e25e11e93cd62901f
harryc73 <harry.connorsle@education.nsw.gov.au>
2024-06-05 09:39:52 +1000

    Halfway Through 5.8

--- Arduino_Button/Arduino_Button.ino
+++ Arduino_Button/Arduino_Button.ino
@@ -1,17 +1,20 @@
 #include "PushButton.h"
 #define Button_PIN 2
 
-PushButton button(Button_PIN); // creating an object of the class and the parameter is the pin number which is 2
-
+PushButton button(Button_PIN, true, true); // creating an object of the class and the parameter is the pin number which is 2
+// true true is there because are using pullup resistor and it is activcated as per the if statement in the void function
 
 void setup() {
   Serial.begin(9600);
   button.init(); //initialise the mode of the pin for the button (no need to worry about which pin is used)
-
 }
 
 void loop() {
-  Serial.println(button.readState()); //reading the state of the board through calling the function in the class
+  if (button.isPressed()){ // calling the is pressed function instead of using read state
+    Serial.println("Button is pressed");
+  }
+  else{
+    Serial.println("Button is not pressed");
+  }
   delay(100);
-
 }
---
+++ Button_and_LED/Button_and_LED.ino
@@ -0,0 +1,19 @@
+#include "Led.h"
+#include "PushButton.h"
+
+#define RED_LED_PIN 13
+#define YELLOW_LED_PIN 12
+#define GREEN_LED_PIN 10
+#define BUTTON_PIN 2
+
+Led reeeeee
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
