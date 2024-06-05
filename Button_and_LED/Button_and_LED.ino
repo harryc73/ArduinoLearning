7ad57604ec6599dfc1d2081f18c0ae1576c9b449
harryc73 <harry.connorsle@education.nsw.gov.au>
2024-06-06 09:07:17 +1000

    Combined Button and Led (Finished Lesson 5)

--- Button_and_LED/Button_and_LED.ino
+++ Button_and_LED/Button_and_LED.ino
@@ -3,17 +3,32 @@
 
 #define RED_LED_PIN 13
 #define YELLOW_LED_PIN 12
-#define GREEN_LED_PIN 10
+#define GREEN_LED_PIN 11
 #define BUTTON_PIN 2
 
-Led reeeeee
+Led redLed(RED_LED_PIN);
+Led yellowLed(YELLOW_LED_PIN);
+Led greenLed(GREEN_LED_PIN);
+PushButton button(BUTTON_PIN, true, true);
 
 void setup() {
-  // put your setup code here, to run once:
+  redLed.init(); // initialising all of the objects 
+  yellowLed.init(); // works well with the LED class
+  greenLed.init(); // overall code provides ease of not having to repeat, classes
+  button.init();
 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop() { // code is super clear
+  if (button.isPressed()){
+    redLed.on();
+    yellowLed.off();
+    greenLed.on();
+  }
+  else{
+    redLed.off();
+    yellowLed.on();
+    greenLed.off();
+  }
 
 }
