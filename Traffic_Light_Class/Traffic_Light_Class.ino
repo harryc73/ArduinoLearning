2da87de37db93e5578223eebd393b35ded94ebb3
harryc73 <harry.connorsle@education.nsw.gov.au>
2024-06-12 14:13:51 +1000

    7.2 Traffic Light Class Begin

---
+++ Traffic_Light_Class/Traffic_Light_Class.ino
@@ -0,0 +1,24 @@
+#include "Led.h"
+#include "LedBlinker.h"
+#include "PushButton.h"
+#include "Traffic_Light.h"
+
+#define RED_LED_PIN 13
+#define YELLOW_LED_PIN 12
+#define GREEN_LED_PIN 11
+#define BUTTON_PIN 2
+
+Led redLed(RED_LED_PIN);
+Led yellowLED(YELLOW_LED_PIN);
+LedBLinker greenLed(greenLed)
+PushButton(BUTTON_PIN, true, true)
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
