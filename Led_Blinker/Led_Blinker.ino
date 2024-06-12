9167eaa429243be5bab5c2bd77da89b179f93c1a
harryc73 <harry.connorsle@education.nsw.gov.au>
2024-06-12 14:00:20 +1000

    Finished LED Blinker

--- Led_Blinker/Led_Blinker.ino
+++ Led_Blinker/Led_Blinker.ino
@@ -1,18 +1,29 @@
 #include "Led.h"
 #include "LedBlinker.h"
 
-#define LED_PIN 11
+#define RED_LED_PIN 13
+#define YELLOW_LED_PIN 12
+#define GREEN_LED_PIN 11
 
-Led led(LED_PIN); // notice how LED_PIN are the paratmeter being passed through, hence making an object passed into the class
-LedBlinker ledBlinker(led);
+Led redLed(RED_LED_PIN); // notice how LED_PIN are the paratmeter being passed through, hence making an object passed into the class
+Led yellowLed(YELLOW_LED_PIN);
+Led greenLed(GREEN_LED_PIN);
 
+
+
+LedBlinker redLedBlinker(redLed); // changing all the parameters and calls to the colour of the LED, not sure why or how, maybe so definition isnt doubled
+LedBlinker yellowLedBlinker(yellowLed); // adding extra parameter as it is the blink delay in milliseconds from the class, and it is a public value 
+LedBlinker greenLedBlinker(greenLed); // the words before the brackets are the objects being created
 
 void setup() {
-  ledBlinker.initLed();
+  redLedBlinker.initLed(); // all objects initialised
+  yellowLedBlinker.initLed();
+  greenLedBlinker.initLed();
   
 }
 
 void loop() {
-  ledBlinker.update(); // no need to pass parameters through it will work simultanously
-
+  redLedBlinker.update(); // no need to pass parameters through it will work simultanously
+  yellowLedBlinker.update();
+  greenLedBlinker.update();
 }
